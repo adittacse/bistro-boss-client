727b2f662e7d9f6a409cfc382bddc25c6ebca2aa
Aditta Chakraborty <adittacse@gmail.com>
2023-05-24 15:40:22 +0600

    implemented moment js & footer added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer.jsx";
 
 const Main = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
