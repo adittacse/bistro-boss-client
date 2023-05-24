1c4bcf5eb2ff1d0c6e39050a307753e1268cc880
Aditta Chakraborty <adittacse@gmail.com>
2023-05-24 15:49:35 +0600

    navbar adde

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Outlet} from "react-router-dom";
 import Footer from "../pages/Shared/Footer/Footer.jsx";
+import NavBar from "../pages/Shared/NavBar/NavBar.jsx";
 
 const Main = () => {
     return (
         <div>
+            <NavBar></NavBar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
