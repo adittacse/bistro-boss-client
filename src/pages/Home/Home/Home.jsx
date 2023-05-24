fbeb16a1378c4c8efa6fcc2aa37441666b6d71fc
Aditta Chakraborty <adittacse@gmail.com>
2023-05-24 23:42:22 +0600

    home page popular menu items displayed and image url updated

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from "../Banner/Banner.jsx";
 import Category from "../Category/Category.jsx";
+import PopularMenu from "../PopularMenu/PopularMenu.jsx";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Category></Category>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
---
+++ src/pages/Shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const MenuItem = ({ item }) => {
+    const {name, recipe, image, price} = item;
+    
+    return (
+        <div className="flex space-x-2">
+            <img style={{width: "118px", height: "104px", borderRadius: "0 200px 200px 200px"}} src={image} alt="Item"/>
+            <div>
+                <h3 className="uppercase">{name} ------------------</h3>
+                <p>{recipe}</p>
+            </div>
+            <p className="text-yellow-500">${price}</p>
+        </div>
+    );
+};
+
+export default MenuItem;
