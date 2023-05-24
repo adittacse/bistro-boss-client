25bcc109c2040d7d593c9b85b3327920f9999ca1
Aditta Chakraborty <adittacse@gmail.com>
2023-05-25 01:18:48 +0600

    featured item added in home page

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from "../Banner/Banner.jsx";
 import Category from "../Category/Category.jsx";
 import PopularMenu from "../PopularMenu/PopularMenu.jsx";
+import Featured from "../Featured/Featured.jsx";
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
             <Banner></Banner>
             <Category></Category>
             <PopularMenu></PopularMenu>
+            <Featured></Featured>
         </div>
     );
 };
