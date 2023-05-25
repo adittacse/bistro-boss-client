3edc925f985c75714bbb43da334b154e11786a28
Aditta Chakraborty <adittacse@gmail.com>
2023-05-25 13:14:39 +0600

    cover added in our menu page and parallax implemented

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import Cover from "../Shared/Cover/Cover.jsx";
+
+import menuImg from "../../assets/menu/banner3.jpg";
+import PopularMenu from "../Home/PopularMenu/PopularMenu.jsx";
 
 const Menu = () => {
     return (
@@ -7,7 +11,16 @@ const Menu = () => {
             <Helmet>
                 <title>Our Menu | Bistro Boss</title>
             </Helmet>
-            <h2>Our Menu</h2>
+            <Cover img={menuImg} title="Our Menu" description="Would you like to try a dish?"></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="Desserts" description="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="Pizza" description="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="Salads" description="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></Cover>
+            <PopularMenu></PopularMenu>
+            <Cover img={menuImg} title="Soups" description="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></Cover>
+            <PopularMenu></PopularMenu>
         </div>
     );
 };
