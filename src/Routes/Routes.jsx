7c563920a720cc5f55d2badfc9e589096a052125
Aditta Chakraborty <adittacse@gmail.com>
2023-05-27 18:30:27 +0600

    login form added and conditional header footer implemented

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from "../Layout/Main.jsx";
 import Home from "../pages/Home/Home/Home.jsx";
 import Menu from "../pages/Menu/Menu/Menu.jsx";
 import Order from "../pages/Order/Order/Order.jsx";
+import Login from "../pages/Login/Login.jsx";
 
 export const router = createBrowserRouter([
     {
@@ -20,6 +21,10 @@ export const router = createBrowserRouter([
             {
                 path: "order/:category",
                 element: <Order></Order>
+            },
+            {
+                path: "/login",
+                element: <Login></Login>
             }
         ]
     },
