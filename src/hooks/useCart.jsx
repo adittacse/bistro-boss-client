3379abc1a888aa205310f7fe5dfb103b3dcc5d46
Aditta Chakraborty <adittacse@gmail.com>
2023-05-30 03:32:25 +0600

    tanstack query implemented on custom hook useCart

---
+++ src/hooks/useCart.jsx
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import {useContext} from "react";
+import {AuthContext} from "../providers/AuthProvider.jsx";
+
+const useCart = email => {
+    const {user} = useContext(AuthContext);
+    
+    const { isLoading, data: cart = [] } = useQuery({
+        queryKey: ['carts', user?.email],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:3000/carts?email=${user?.email}`);
+            return res.json();
+        },
+    })
+    
+    return [cart, isLoading];
+}
+
+export default useCart;
