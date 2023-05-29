ee62257f7002c4c104133c895e3ce167151f3f7a
Aditta Chakraborty <adittacse@gmail.com>
2023-05-29 10:30:18 +0600

    login page design updated

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -61,12 +61,8 @@ const Login = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center md:w-1/2 lg:text-left">
-                    <h1 className="text-5xl font-bold">Login</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    {/*<img src={img} alt=""/>*/}
-                </div>
                 <div className="card md:w-1/2 max-w-sm shadow-2xl bg-base-100">
+                    <h1 className="text-5xl font-bold text-center mt-6">Login</h1>
                     <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
@@ -108,6 +104,9 @@ const Login = () => {
                             <FaGithub></FaGithub>
                         </button>
                     </div>
+                </div>
+                <div className="text-center md:w-1/2 lg:text-left">
+                    <img src={img} alt=""/>
                 </div>
             </div>
         </div>
