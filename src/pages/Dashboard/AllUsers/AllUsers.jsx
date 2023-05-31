9c8086f54e17548f84377cc6713dce1edb6e955e
Aditta Chakraborty <adittacse@gmail.com>
2023-05-31 20:29:29 +0600

    implemented make a user to admin

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
 import {useQuery} from "@tanstack/react-query";
 import PageHeading from "../../Shared/PageHeading/PageHeading.jsx";
+import { MdAdminPanelSettings } from "react-icons/md";
 import { HiUserGroup } from "react-icons/hi";
 import {RiDeleteBin5Line} from "react-icons/ri";
 import {Helmet} from "react-helmet-async";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
     const {data: users = [], refetch} = useQuery(["users"], async () => {
         const res = await fetch("http://localhost:3000/users");
         return res.json();
     });
+    
+    const handleMakeAdmin = (user) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: `Make ${user.name} as Admin?`,
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, Make Admin!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:3000/users/admin/${user._id}`, {
+                    method: "PATCH"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.modifiedCount) {
+                            refetch();
+                            Swal.fire(
+                                'Congratulations!',
+                                `${user.name} is an Admin now`,
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })
+    }
+    
+    const handleUserDelete = (user) => {
+    
+    }
     
     return (
         <div>
@@ -34,17 +69,25 @@ const AllUsers = () => {
                     </thead>
                     <tbody>
                     {
-                        users.map((user, index) => <tr>
+                        users.map((user, index) => <tr key={user._id}>
                             <th>{index + 1}</th>
                             <td>{user.name}</td>
                             <td>{user.email}</td>
                             <td>
-                                <button className="btn text-lg bg-[#D1A054] text-white">
-                                    <HiUserGroup></HiUserGroup>
-                                </button>
+                                {
+                                    user.role === "admin" ? <>
+                                        <button className="btn text-lg bg-emerald-800 text-white">
+                                            <MdAdminPanelSettings></MdAdminPanelSettings>
+                                        </button>
+                                        </> : <>
+                                            <button onClick={() => handleMakeAdmin(user)} className="btn text-lg bg-[#D1A054] text-white">
+                                                <HiUserGroup></HiUserGroup>
+                                            </button>
+                                        </>
+                                }
                             </td>
                             <td>
-                                <button className="btn text-lg bg-red-800 text-white">
+                                <button onClick={() => handleUserDelete(user)} className="btn text-lg bg-red-800 text-white">
                                     <RiDeleteBin5Line></RiDeleteBin5Line>
                                 </button>
                             </td>
