be33c5d5442092067bf7ec817109ba6ebafc69c3
Aditta Chakraborty <adittacse@gmail.com>
2023-06-01 21:31:52 +0600

    admin private route created

---
+++ src/Routes/AdminRoute.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Navigate, useLocation} from "react-router-dom";
+import useAuth from "../hooks/useAuth.jsx";
+import useAdmin from "../hooks/useAdmin.jsx";
+
+const AdminRoute = ({ children }) => {
+    const {user, loading} = useAuth();
+    const [isAdmin, isAdminLoading] = useAdmin();
+    const location = useLocation();
+    
+    if (loading || isAdminLoading) {
+        return <progress className="progress progress-error w-56 flex mx-auto"></progress>;
+    }
+    
+    if (user && isAdmin) {
+        return children;
+    }
+    
+    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default AdminRoute;
